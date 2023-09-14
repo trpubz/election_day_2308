c1011c5ca21525196bb65e528308f76ff2773ea6
Taylor Pubins <tpubz@icloud.com>
2023-09-14 14:22:42 -0700

    [lib/race.rb][spec/race_spec.rb] Added methods to open/close a race, determine winner and check for ties

--- lib/race.rb
+++ lib/race.rb
@@ -6,10 +6,36 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(candidate)
     @candidates << Candidate.new(candidate)
     @candidates.last
+  end
+
+  def open?
+    @open
+  end
+
+  def close!
+    @open = false
+  end
+
+  def winner
+    if open?
+      false
+    else
+      @candidates.max_by { |candidate| candidate.votes }
+    end
+  end
+
+  def tie?
+    if open?
+      puts "race is still open"
+    else
+      highest = @candidates.max_by(2) { |candidate| candidate.votes }
+      highest.reduce { |a, b| a.votes == b.votes }
+    end
   end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 RSpec.describe Race do
   before(:each) do
     @race = Race.new("Texas Governor")
+    @candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+    @candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
   end
 
   describe "#initialize" do
@@ -12,18 +14,50 @@ RSpec.describe Race do
 
     it "has @attr" do
       expect(@race.office).to eq "Texas Governor"
-      expect(@race.candidates).to eq []
+      expect(@race.candidates).to be_a Array
+      expect(@race.open?).to eq true
     end
   end
 
   describe "#register_candidate!" do
     it "registers & returns ::candidates" do
-      candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
-      expect(candidate1).to be_a Candidate
-      expect(candidate1.name).to eq "Diana D"
-      expect(candidate1.party).to eq :democrat
-      candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
-      expect(@race.candidates).to eq [candidate1 ,candidate2]
+      expect(@candidate1).to be_a Candidate
+      expect(@candidate1.name).to eq "Diana D"
+      expect(@candidate1.party).to eq :democrat
+      expect(@race.candidates).to eq [@candidate1, @candidate2]
+    end
+  end
+
+  describe "#close!" do
+    it "closes the race" do
+      @race.close!
+      expect(@race.open?).to eq false
+    end
+  end
+
+  describe "#winner" do
+    it "returns candidate with most votes if race != open" do
+      expect(@race.winner).to eq false
+      3.times { @candidate2.vote_for! }
+      2.times { @candidate1.vote_for! }
+      @race.close!
+      expect(@race.winner).to eq @candidate2
+    end
+  end
+
+  describe "tie?" do
+    it "returns true if >1 candidate has highest votes" do
+      3.times { @candidate2.vote_for! }
+      3.times { @candidate1.vote_for! }
+      @race.close!
+      expect(@race.tie?).to eq true
+    end
+
+    it "returns false if 1 candidate has highest votes" do
+      3.times { @candidate2.vote_for! }
+      2.times { @candidate1.vote_for! }
+      @race.close!
+      expect(@race.tie?).to eq false
     end
   end
 end
